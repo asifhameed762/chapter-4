829cfeabfe840ef147bd951f7f9296e2eb66534b
Asif Asif <130058986+asifhameed762@users.noreply.github.com>
2024-03-11 17:27:58 +0500

    chapter 4

---
+++ app.js
@@ -0,0 +1,28 @@
+//Q1.
+let name1 = "Asif" , name2 = "samad" , name3 = "Abdul";
+
+//Q2.
+// legal variables
+// 1. let myVar = 20 ;  camel case
+// 2. let alpha12 = 10; 
+// 3. let my_var = 5;
+// 4. let _myvar = 10;
+// 5. let $myvar = 5;
+
+//Illegar variables
+// 1. let my var = 20; not a space 
+// 2. let 12myvar = 10 ; cannot start digit 
+// 3. let class = 10; reserve keyword
+// 4. let my-var = 5 ; cannot hyphen
+// 5. let true = 4  ; reserve keyword
+
+//Q3.
+document.write("<h1>Rules for naming js variables</h1> <br>");
+document.write("Variable names can only contain ,numbers ,$ and _.For example :$my_1stVariable <br>");
+document.write("Variable must begin with a letter,$ or _.For example : $name , _name or name <br>");
+document.write("Variable names are case sensitive <br>");
+document.write("Variable names should not be JS keywords <br>");
+
+
+
+
